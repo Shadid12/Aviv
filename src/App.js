3ca2e0538ea7470afa54fae8a31eab7fc4f77e07
shadid12 <shadidhaque2012@gmail.com>
2018-08-31 23:25:30 -0400

    added new router

--- src/App.js
+++ src/App.js
@@ -6,20 +6,48 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 
+import { BrowserRouter as Router, Route, Link, withRouter  } from "react-router-dom";
+import { Button } from "@material-ui/core";
+
+
+const New = withRouter(({ history }) => (
+  <Button
+    type='button'
+    onClick={() => { history.push('/new') }}
+  >
+    New Part
+  </Button>
+))
+
+const Home = withRouter(({ history }) => (
+  <MenuIcon
+    onClick={() => { history.push('/') }}
+  >
+  </MenuIcon>
+))
 
 export default class App extends React.Component {
   render() {
     return(
       <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Create />
+        <Router>
+          <div>
+            <AppBar position="static">
+              <Toolbar variant="dense">
+                <IconButton color="inherit" aria-label="Menu">
+                  <Home />
+                </IconButton>
+                {/* links  */}
+
+                <New />
+              </Toolbar>
+            </AppBar>
+            <Route path="/new" component={Create} />
+            {/* <Create /> */}
+          </div>
+        </Router>
       </div>
     )
   }
